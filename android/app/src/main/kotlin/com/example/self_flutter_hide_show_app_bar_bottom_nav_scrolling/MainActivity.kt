e9e49fedd3843879058ee25ba9aa34f6fa43d19e
MinaSamir <MenaSamerCap@gmail.com>
2022-05-25 12:30:13 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/self_flutter_hide_show_app_bar_bottom_nav_scrolling/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.self_flutter_hide_show_app_bar_bottom_nav_scrolling
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String title = "Hide Show Appbar Bottom Nav";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Text(title),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: "Dummy",
+        child: const Icon(Icons.title),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
